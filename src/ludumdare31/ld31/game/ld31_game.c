b6451b477e2952b43d0f65d0854a6e96400459ad
Paul <crazy046@hotmail.co.uk>
2014-12-06 09:54:37 +0000

    two players

--- src/ludumdare31/ld31/game/ld31_game.c
+++ src/ludumdare31/ld31/game/ld31_game.c
@@ -20,6 +20,7 @@ void play_game(Ld31_game *game) {
 
 	Ld31_level *level = load_level(0, game);
 	entity *e = create_entity("player", SSL_Image_Load("../extras/resources/sprites/player.png", 32, 32, game->window), up, 100, 100);
+	entity *e2 = create_entity("player", SSL_Image_Load("../extras/resources/sprites/player.png", 32, 32, game->window), up, 100, 100);
 
 	while (running) {
 		SDL_RenderPresent(game->window->renderer);
@@ -27,6 +28,7 @@ void play_game(Ld31_game *game) {
 
 		SSL_Tiled_Draw_Map(level->map, 0, 0, game->window);
 		SSL_Image_Draw(e->image, e->x, e->y, 0, 0, SDL_FLIP_NONE, game->window);
+		SSL_Image_Draw(e2->image, e2->x, e2->y, 0, 0, SDL_FLIP_NONE, game->window);
 
 		if (SSL_Keybord_Keyname_Down("_a")) {
 			e->x -= 1;
@@ -39,6 +41,19 @@ void play_game(Ld31_game *game) {
 			}
 		if (SSL_Keybord_Keyname_Down("_w")) {
 				e->y -= 1;
+			}
+
+		if (SSL_Keybord_Keyname_Down("_left")) {
+			e2->x -= 1;
+		}
+		if (SSL_Keybord_Keyname_Down("_right")) {
+				e2->x += 1;
+			}
+		if (SSL_Keybord_Keyname_Down("_down")) {
+				e2->y += 1;
+			}
+		if (SSL_Keybord_Keyname_Down("_up")) {
+				e2->y -= 1;
 			}
 
 		while(SDL_PollEvent(&event)) {
